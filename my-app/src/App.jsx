e1007fe5cc9c2b9bb37356cbbed11d0d42e47e54
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-08-17 21:14:57 +0600

    commit-2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-
-      
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Student />
+      <Devloper />
     </>
   )
 }
@@ -21,14 +21,39 @@ function App() {
 function Person() {
   const age = 25;
   const money = 2000;
-  const person ={name:'abid', age:12}
-  return  ( 
-  <>
-   <h3>I am a person with age: {age} and money: {money}</h3>
-   <h3>I am {person.name} with {age}</h3>
-   </>
-   )
+  const person = { name: 'abid', age: 12 }
+  return (
+    <>
+      <h3>I am a person with age: {age} and money: {money}</h3>
+      <h3>I am {person.name} with {age}</h3>
+    </>
+  )
+}
 
+function Student() {
+  return (
+    <div className='student'>
+      <h3>This is student</h3>
+      <p>Name:</p>
+      <p>Age:</p>
+    </div>
+  )
+}
+
+function Devloper() {
+  const devloperstyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px',
+  }
+  return (
+    <div style={devloperstyle}>
+      <h3>Go to Dhaka</h3>
+      <p>Dhaka is a beautiful city</p>
+    </div>
+  )
 }
 
 export default App
+
