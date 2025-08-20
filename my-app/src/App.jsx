ccb5bf74a9fa3459b840b1dad4daaad8283ed755
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-08-20 23:51:04 +0600

    commit-4

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Tool from './tool'
+import BookStore from './Books'
 
 function App() {
   return (
---
+++ my-app/src/Books.jsx
@@ -0,0 +1,11 @@
+import './Book.css';
+
+
+export default function BookStore({books}){
+    return(
+      <div>
+        <button className="">Click Me</button>
+      </div>
+    )
+
+}
